9e15ae9431028c93a46cbe0cdb409b28c9eea5c9
davidcawork <davidcawork@gmail.com>
2019-02-09 12:43:17 +0100

    recv and send func

--- Ping_over_TCP-UDP/C_version/Ping_over_TCP(OC)/ping_oc.c
+++ Ping_over_TCP-UDP/C_version/Ping_over_TCP(OC)/ping_oc.c
@@ -1,10 +1,19 @@
 /*
  * Copyright (C) 2018 David Carrascal(1);
-
- *                    (1) GIST, University of Alcala, Spain.
+ * 
+ *                    (1) University of Alcala, Spain.
  *                    
- *  Ping tool over tcp 
+ *  
+ *  Ping tool over tcp     
+ *    
+ *  Usage : ./ping_oc.out <IP/destination> <Port>
+ *   
+ *  For more information you can check: github.com/davidcawork/uah-lrss_/tree/master/Ping_over_TCP-UDP/C_version
+ * 
+ * 
  */
+
+
 
 #include <stdio.h>      /*  for printf() ... */
 #include <stdlib.h>     /*  for atoi(), exit(), malloc(), free()*/
@@ -17,10 +26,14 @@
 #include <signal.h>     /*  To  handle CTRL+C signal(sigint)    */
 #include <stdbool.h>    /*  for use true/false and type bool */
 #include <string.h>     /*  for memset()    */
+#include <unistd.h>     /*  for close()     */
+
 
 #define BUFFER_SIZE 256
-#define INTERVAL_REQUEST 2000 /* units: ms (2s) */
-
+#define INTERVAL_REQUEST 2000 /* units: ms (2s)  Windows use 4sec() and Linux 1 for non-super user(Source: manual page(ping))*/
+#define REQUEST "Ping : Request (8)\n"
+ 
+/*  Main data structure for make a ping request */
 typedef struct ping{
 
     char * server_name;
@@ -28,47 +41,53 @@ typedef struct ping{
     int socket;
     struct sockaddr_in server;
     char buffer[BUFFER_SIZE];
-    int pings_sended;
-    int data_send, data_recv; //Units : bytes
+    int pings_sended,pings_lost;
+    ssize_t data_send, data_recv; //Units : bytes
     struct hostent * resolv;
     
 } ping_t;
 
+/*  Fuctions Declaration    */
 volatile bool shouldKeep_pinging = true;
+void CTRLDhandler(int a);
+void Send_ping(ping_t * , char *, ssize_t *, clock_t * );
+void waitReply(ping_t * ,ssize_t *, clock_t * , clock_t * );
+int  IntervalTimer(clock_t * , clock_t *);
+void StatisticsPing(ping_t *, char *);
 
-void CTRLDhandler(int a){
-    shouldKeep_pinging = false;
-}
-
-/*  Usage : ./a.out IP/destination Port */
 
 int main(int argc, char * argv []){
 
     /*  Var.aux */
     ping_t pet_ping;
-    char ping_request[]= {"Ping : Request (8)"};
+    char ping_request[]= {REQUEST};
     int timer_ms = 0;
+    clock_t interval,init_send;
+    ssize_t n_data_ = 0;
+    
+    /*  ----    Here starts our program     ----  */
 
     /* Prepare CTRL+C exit */
     if(signal(SIGINT, &CTRLDhandler) == SIG_ERR){
         printf("Error: cannot set signal handler\n");
         exit(1);
     }
-
+    
     /*  Check arg's   */
     if(argc < 3){
-        printf("Error, usage: %s destination port\n",argv[0]);
+        printf("Error: Usage: %s <destination> <port>\n",argv[0]);
         exit(1);
     }else{
-
+        
         /*  DNS query */
         pet_ping.resolv = gethostbyname(argv[1]);
         if((pet_ping.resolv) == NULL){
-           printf("Cannot find Ip\n"); 
+           printf("Error: cannot find Ip\n"); 
            exit(1);
         }else{  
-
+            
             pet_ping.pings_sended = 0;
+            pet_ping.pings_lost=0;
             pet_ping.data_send = 0;
             pet_ping.data_recv = 0;
             pet_ping.server_name = pet_ping.resolv->h_addr_list[0];
@@ -80,7 +99,7 @@ int main(int argc, char * argv []){
             pet_ping.server.sin_port = htons(atoi(pet_ping.port)); /* To big Endian   */
 
             if(inet_pton(AF_INET, pet_ping.server_name, &pet_ping.server.sin_addr.s_addr)){
-                printf("Error: cannot convert Ip/destination into a 32-bit binary repr\n");
+                printf("Error: cannot convert <Ip/destination> into a 32-bit binary repr\n");
                 exit(1);
             }
 
@@ -96,13 +115,100 @@ int main(int argc, char * argv []){
                 exit(1);
             }
 
+            /* First Ping */
+            Send_ping(&pet_ping, ping_request, &n_data_,&init_send);
+
             while(shouldKeep_pinging){
-                //Send_ping(&pet_ping, ping_request, );
+
+                /*    Get Interval    */
+                timer_ms = IntervalTimer(&interval,&init_send);
+
+                /*  Should we ping again, the interval has reached? */
+                if(timer_ms >= INTERVAL_REQUEST){   
+                    Send_ping(&pet_ping, ping_request, &n_data_,&init_send);
+                }
+
+                waitReply(&pet_ping,&n_data_,&interval,&init_send);   
+
             }
 
-            printf("\n\n\nEstadisticas de ping\n");
+            
+            StatisticsPing(&pet_ping,argv[1]);
+            close(pet_ping.socket);
         }
 
        
     }
+
+    return 0;
+}
+
+/*  Fuctions definitions   */
+
+void CTRLDhandler(int a){
+   
+    shouldKeep_pinging = false;
+}
+
+void Send_ping(ping_t * pet, char * request, ssize_t * n_data_sent,clock_t * init_send){
+
+    *(n_data_sent) = send(pet->socket, request,strlen(request), 0);
+    *(init_send) = clock();
+    if(*(n_data_sent) < 0){
+        printf("Error: cannot send a request\n");
+        exit(1);
+    }else{
+        pet->pings_sended++;
+        pet->data_send += *(n_data_sent) ;
+    }
+}
+
+void waitReply(ping_t * pet, ssize_t * n_data_recv, clock_t * interval, clock_t * init_send){
+    /*  Var.aux */
+    size_t total_data_rcv = 0;
+
+    /*
+     *  Let's assume that if we don't receive a response before 90% of the send interval, 
+     *  the ping has been lost. 
+     *  
+     *  With this approach we try that there are no race conditions at the time of sending and receiving pings,
+     *  and that the send interval is respected. 
+     *  
+     *  In addition, to not complicating the program anymore, otherwise we would have to add another counter 
+     *  that is responsible for establishing if a package is has lost. 
+     *  
+     *  :)
+     * 
+     */
+
+    while((IntervalTimer(interval,init_send) < (0.9*INTERVAL_REQUEST))    ||   (total_data_rcv  < (strlen(REQUEST))) ){
+        
+        *(n_data_recv) = recv(pet->socket,pet->buffer,BUFFER_SIZE -1 , 0);
+        if(*(n_data_recv) < 0){
+            printf("Error: cannot recv data\n");
+            exit(1);
+        }
+
+        total_data_rcv += *(n_data_recv);
+        pet->buffer[*n_data_recv] = '\0';
+        pet->data_recv += *(n_data_recv);
+
+    }
+
+    if(total_data_rcv  == (strlen(REQUEST))){
+        printf("%d bytes from %s (%s): time= %d (ms)\n", total_data_rcv, pet->resolv->h_name,inet_ntoa(*((struct in_addr *) pet->resolv->h_addr_list[0])),IntervalTimer(interval,init_send));
+    }else{
+        pet->pings_lost++;
+    }
+}
+
+int  IntervalTimer(clock_t * interval, clock_t * init_send){
+    *interval = clock() - *init_send;
+    return (((*interval) * 1000) / CLOCKS_PER_SEC);
+}
+
+void StatisticsPing(ping_t * pet, char * name){
+
+    printf("--- %s ping statistics ---\n",name);
+    printf("%d packets transmitted, %d received, %f %c packet loss\n",pet->pings_sended, (pet->pings_sended - pet->pings_lost), 100*((float)(pet->pings_lost)/pet->pings_sended),0x25);
 }
